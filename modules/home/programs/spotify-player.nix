543721f0cd1fc9671ea00ea7ca6dfce7c6c30c75
Robert Babaev <github@robertbabaev.tech>
2025-12-02 10:21:45 -0500

    fix: identify correct audio backend for macos
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/programs/spotify-player.nix
+++ modules/home/programs/spotify-player.nix
@@ -1,11 +1,11 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   programs.spotify-player = {
     enable = true;
     package = pkgs.spotify-player.override {
       withAudioBackend =
         if pkgs.stdenv.isLinux # This fixed an issue on EndeavourOS
         then "pulseaudio"
-        else "pulseaudio";
+        else "portaudio";
     };
   };
 }
