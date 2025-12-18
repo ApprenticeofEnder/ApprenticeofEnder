f71536154f983591fad78e8bd7b3a384f0294a1a
Robert Babaev <github@robertbabaev.tech>
2025-12-17 22:06:21 -0500

    build: add vscode
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

---
+++ modules/home/programs/vscode.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}: {
+  programs.vscode = {
+    enable = true;
+    profiles = {
+      default = {
+        extensions = with pkgs.vscode-extensions; [
+          bbenoist.nix
+        ];
+      };
+    };
+  };
+}
