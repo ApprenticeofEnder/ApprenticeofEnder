63a0274dfc9b6ab8204d7d0756b78e3f9cc8b1e6
Robert Babaev <github@robertbabaev.tech>
2025-12-28 13:05:56 -0500

    feat(programs/vscode): add more extensions
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/programs/linux-only/vscode.nix
+++ modules/home/programs/linux-only/vscode.nix
@@ -5,7 +5,34 @@
     profiles = {
       default = {
         extensions = with pkgs.vscode-extensions; [
+          # languages
+          vue.volar
           bbenoist.nix
+          hashicorp.hcl
+          redhat.ansible
+          ms-python.python
+          hashicorp.terraform
+          svelte.svelte-vscode
+          rust-lang.rust-analyzer
+
+          # formatters and linters
+          mkhl.shfmt
+          biomejs.biome
+          charliermarsh.ruff
+          dbaeumer.vscode-eslint
+          esbenp.prettier-vscode
+          detachhead.basedpyright
+          formulahendry.auto-close-tag
+
+          # theme
+          pkief.material-icon-theme
+          pkief.material-product-icons
+          arcticicestudio.nord-visual-studio-code
+
+          # utilities
+          docker.docker
+          asvetliakov.vscode-neovim
+          github.vscode-github-actions
         ];
       };
     };
