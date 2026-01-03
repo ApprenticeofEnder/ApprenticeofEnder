882dd6577c3050e56a8636d515c8a950803f3532
Robert Babaev <github@robertbabaev.tech>
2026-01-03 03:56:04 -0500

    build(programs/linux_only): add scaffolding for obsidian
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

---
+++ modules/home/programs/linux-only/obsidian.nix
@@ -0,0 +1,24 @@
+{
+  programs.obsidian = {
+    enable = false;
+    defaultSettings = {
+      app = {
+      };
+      themes = [
+      ];
+      hotkeys = {
+      };
+      appearance = {
+      };
+      extraFiles = {
+      };
+    };
+    vaults = {
+      hub = {
+        enable = false;
+        # target = "Documents/Hub";
+        # settings = {};
+      };
+    };
+  };
+}
