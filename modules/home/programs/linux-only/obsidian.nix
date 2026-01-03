d1e29a0728b6b2b0af9f452b2715cc9536245350
Robert Babaev <github@robertbabaev.tech>
2026-01-03 04:16:57 -0500

    docs: references
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/programs/linux-only/obsidian.nix
+++ modules/home/programs/linux-only/obsidian.nix
@@ -4,6 +4,27 @@
     defaultSettings = {
       app = {
       };
+
+      # app = {
+      #   "showInlineTitle": true,
+      #   "newFileLocation": "folder",
+      #   "newFileFolderPath": "Notes",
+      #   "newLinkFormat": "shortest",
+      #   "attachmentFolderPath": "_system/Attachments",
+      #   "promptDelete": false,
+      #   "readableLineLength": false,
+      #   "pdfExportSettings": {
+      #     "includeName": true,
+      #     "pageSize": "Letter",
+      #     "landscape": false,
+      #     "margin": "0",
+      #     "downscalePercent": 100
+      #   },
+      #   "vimMode": true,
+      #   "alwaysUpdateLinks": true
+      # };
+      communityPlugins = {
+      };
       themes = [
       ];
       hotkeys = {
