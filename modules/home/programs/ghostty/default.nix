96903bf668610b920f01f28f7337cf0c6b777cf2
Robert Babaev <github@robertbabaev.tech>
2026-01-05 22:19:42 -0500

    feat(programs): update ghostty config for near parity with kitty
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/programs/ghostty/default.nix
+++ modules/home/programs/ghostty/default.nix
@@ -1,5 +1,35 @@
 {...}: {
   programs.ghostty = {
     enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    settings = {
+      scrollback-limit = 10000;
+      window-decoration = "client";
+      window-height = 3000;
+      window-width = 3000;
+
+      theme = "Nord";
+      font-size = 11;
+      font-family = "Hack Nerd Font";
+      keybind = [
+        "ctrl+w>v=new_split:right"
+        "ctrl+w>s=new_split:down"
+        "ctrl+h=goto_split:left"
+        "ctrl+l=goto_split:right"
+        "ctrl+k=goto_split:up"
+        "ctrl+j=goto_split:down"
+        "ctrl+shift+v=paste_from_selection"
+      ];
+
+      copy-on-select = "clipboard";
+      env = [
+        "LS_COLORS=1"
+      ];
+
+      macos-option-as-alt = true;
+
+      working-directory = "inherit";
+    };
   };
 }
