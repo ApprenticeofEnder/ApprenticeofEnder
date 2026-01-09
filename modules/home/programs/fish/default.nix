0a8eceecd4439df674428c71837af6502564de17
Robert Babaev <github@robertbabaev.tech>
2026-01-09 15:20:11 -0500

    feat: add gh cli plugin
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/programs/fish/default.nix
+++ modules/home/programs/fish/default.nix
@@ -4,18 +4,6 @@
   ...
 }:
 lib.mkMerge [
-  {
-    programs.fish = {
-      enable = true;
-      shellAliases = {
-        mkdir = "mkdir -p";
-      };
-      shellInit = ''
-        fish_add_path ~/.local/bin
-        source "$HOME/.config/op/plugins-nix.sh"
-      '';
-    };
-  }
   (lib.mkIf pkgs.stdenv.isDarwin {
     programs.fish = {
       shellInit = ''
@@ -24,4 +12,17 @@ lib.mkMerge [
       '';
     };
   })
+  {
+    programs.fish = {
+      enable = true;
+      shellAliases = {
+        mkdir = "mkdir -p";
+      };
+      shellInit = ''
+        fish_add_path ~/.local/bin
+        fish_add_path ~/.nix-profile/bin
+        source "$HOME/.config/op/plugins-nix.sh"
+      '';
+    };
+  }
 ]
