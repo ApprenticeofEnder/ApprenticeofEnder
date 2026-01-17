de1bf5a704790e26b57d746ef08ca53c236846bf
Robert Babaev <github@robertbabaev.tech>
2026-01-16 22:05:56 -0500

    build: add udev rules config for reader symlinks
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/programs/linux-only/abcde.nix
+++ modules/home/programs/linux-only/abcde.nix
@@ -1,14 +1,30 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  readers = {
+    robDay = {
+      devFile = "cd-rday";
+      idSerialShort = "";
+    };
+  };
+in {
   home = {
     packages = with pkgs; [
       abcde
     ];
+
+    file = {
+      "/etc/udev/rules.d/cd.rules" = {
+        text = ''
+          ACTION=="add", KERNEL=="sr[0-9]", ENV{ID_SERIAL_SHORT}=="${readers.robDay.idSerialShort}", SYMLINK+="${readers.robDay.devFile}"
+        '';
+      };
+    };
   };
 
+  # TODO: Maybe make a config file for udev rules?
   programs.fish = {
     functions = {
       cdrip = ''
-        abcde -o flac -B && notify-send "Rip Complete" "Your CD rip is finished!"
+        abcde -o flac -B -d /dev/${readers.robDay.devFile} && notify-send "Rip Complete" "Your CD rip is finished!"
       '';
     };
   };
