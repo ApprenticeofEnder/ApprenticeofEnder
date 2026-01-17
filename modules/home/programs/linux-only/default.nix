96358f706ca0208b22e0de12f952c353ed4d9762
Robert Babaev <github@robertbabaev.tech>
2026-01-17 02:32:47 -0500

    fix: why did opus say this was valid
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/programs/linux-only/default.nix
+++ modules/home/programs/linux-only/default.nix
@@ -1,5 +1,5 @@
 # A module that automatically imports everything else in the parent folder.
 {
   imports = with builtins;
-    map (fn: ./${fn}) (filter (fn: fn != "default.nix" && hasSuffix ".nix" fn) (attrNames (readDir ./.)));
+    map (fn: ./${fn}) (filter (fn: fn != "default.nix") (attrNames (readDir ./.)));
 }
