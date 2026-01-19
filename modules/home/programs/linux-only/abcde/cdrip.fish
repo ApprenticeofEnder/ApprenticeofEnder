b33085bdc082ac4a5d3079e4b1a2e562109188c7
Robert Babaev <github@robertbabaev.tech>
2026-01-19 00:54:36 -0500

    fix(programs/abcde): quote variables
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/programs/linux-only/abcde/cdrip.fish
+++ modules/home/programs/linux-only/abcde/cdrip.fish
@@ -1,20 +1,25 @@
 function failed
-    ntfy publish $ntfyTopic "$(cat rip.log)"
+    ntfy publish "$ntfyTopic" "$(cat rip.log)"
     return 1
 end
 
-ntfy publish $ntfyTopic "CD rip starting..."
+if test -n "$ntfyTopic"
+    echo "NTFY topic not set. Exiting."
+    return 2
+end
+
+ntfy publish "$ntfyTopic" "CD rip starting..."
 abcde $argv &>rip.log
 set abcde_success $status
 rg -q "SSL connect attempt failed" rip.log
 set ssl_failed $status
 
-if test $ssl_failed
+if test "$ssl_failed"
     failed
 end
 
-if test $abcde_success
-    ntfy publish $ntfyTopic "CD rip complete."
+if test "$abcde_success"
+    ntfy publish "$ntfyTopic" "CD rip complete."
 else
     failed
 end
