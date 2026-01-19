4f9934611743b9becb201a5302f834336693e7ff
Robert Babaev <github@robertbabaev.tech>
2026-01-19 03:09:54 -0500

    feat(programs/abcde): usability improvement
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/programs/linux-only/abcde/migrate.fish
+++ modules/home/programs/linux-only/abcde/migrate.fish
@@ -1,9 +1,8 @@
 for folder in (fd -d 1 -t d '-')
-    echo $folder
     set split_folder (string split - $folder)
     set artist $split_folder[1]
     set album $split_folder[2]
-    echo $artist $album
+    echo "Moving '$folder' to '$artist/$album'"
     mkdir $artist || echo "Folder exists: $artist"
     mv $folder "$artist/$album"
 end
