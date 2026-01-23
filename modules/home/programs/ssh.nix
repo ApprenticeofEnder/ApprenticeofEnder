56a4245fcc5ffe789e9c42bb41c209b2d256ac3f
Robert Babaev <github@robertbabaev.tech>
2026-01-23 12:37:48 -0500

    chore(ssh): remove old ssh hosts
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/programs/ssh.nix
+++ modules/home/programs/ssh.nix
@@ -58,39 +58,6 @@ in {
           identityFile = ["~/.ssh/github.pub"];
         };
 
-      rabbit-holes =
-        hostDefaults
-        // {
-          user = "vpcadmin";
-          host = "rabbit-holes.ctf";
-          hostname = "10.0.2.51";
-          extraOptions = {
-            strictHostKeyChecking = "no";
-          };
-          identityFile = ["~/.ssh/cybersci_2025_regionals.pub"];
-        };
-
-      trc =
-        hostDefaults
-        // {
-          user = "vpcadmin";
-          host = "trc.ctf";
-          hostname = "10.0.2.41";
-          extraOptions = {
-            strictHostKeyChecking = "no";
-          };
-          identityFile = ["~/.ssh/cybersci_2025_regionals.pub"];
-        };
-
-      defence =
-        hostDefaults
-        // {
-          user = "vpcadmin";
-          host = "inventory.ctf";
-          hostname = "10.0.2.22";
-          identityFile = ["~/.ssh/cybersci_2025_regionals.pub"];
-        };
-
       homelab-pi =
         hostDefaults
         // {
@@ -100,88 +67,78 @@ in {
           identityFile = ["~/.ssh/pi_master.pub"];
         };
 
-      nixos = {
-        port = 22;
-        host = "nixos";
-        user = "ender";
-        checkHostIP = true;
-        addKeysToAgent = "yes";
-        hostname = "192.168.64.6";
-        setEnv.TERM = "xterm-kitty";
-      };
-
-      rpi5 = {
-        port = 22;
-        host = "rpi5";
-        user = "ender";
-        checkHostIP = true;
-        addKeysToAgent = "yes";
-        hostname = "192.168.50.241";
-        setEnv = {
-          TERM = "xterm-256color";
-        };
-
-        # addressFamily = null; # "any" | "inet" | "inet6"
-        # certificateFile = [ ./.file ];
-
-        # compression = false;
-        # controlmaster = null; # "yes" | "no" | "ask" | "auto" | "autoask"
-        # controlPath = null; # path to control socket used for connection sharing
-        # controlPersist = "10am"; # whether control socket should remain open in background
-
-        # identityFile = [];
-        # identityAgent = [];
-        # identitiesOnly = false;
-
-        # hashKnownHosts = null;
-        # userKnownHostsFile = "~/.ssh/known_hosts";
-
-        # serverAliveInterval = 5;
-        # serverAliveCountMax = 5;
-
-        # proxyJump = "";
-        # proxyCommand = "";
-
-        #  match = ''
-        #  host  canonical
-        #  host  exec "ping -c1 -q 192.168.17.1"
-        # '';
-
-        # dynamicForwards  = [
-        #   {
-        #     "name" = {
-        #       address = "localhost";
-        #       port = 8080;
-        #     };
-        #   }
-        # ];
-
-        # remoteForwards = [
-        #   {
-        #     bind = {
-        #       address = "10.0.0.13";
-        #       port = 8080;
-        #     };
-        #     host = {
-        #       address = "10.0.0.13";
-        #       port = 80;
-        #     };
-        #   }
-        # ];
-
-        # localForwards = [
-        #   {
-        #     bind = {
-        #       address = "10.0.0.13";
-        #       port = "8080";
-        #     };
-        #     host = {
-        #       address = "10.0.0.13";
-        #       port = "80";
-        #     };
-        #   }
-        # ];
-      };
+      # rpi5 = {
+      #   port = 22;
+      #   host = "rpi5";
+      #   user = "ender";
+      #   checkHostIP = true;
+      #   addKeysToAgent = "yes";
+      #   hostname = "192.168.50.241";
+      #   setEnv = {
+      #     TERM = "xterm-256color";
+      #   };
+      #
+      #   # addressFamily = null; # "any" | "inet" | "inet6"
+      #   # certificateFile = [ ./.file ];
+      #
+      #   # compression = false;
+      #   # controlmaster = null; # "yes" | "no" | "ask" | "auto" | "autoask"
+      #   # controlPath = null; # path to control socket used for connection sharing
+      #   # controlPersist = "10am"; # whether control socket should remain open in background
+      #
+      #   # identityFile = [];
+      #   # identityAgent = [];
+      #   # identitiesOnly = false;
+      #
+      #   # hashKnownHosts = null;
+      #   # userKnownHostsFile = "~/.ssh/known_hosts";
+      #
+      #   # serverAliveInterval = 5;
+      #   # serverAliveCountMax = 5;
+      #
+      #   # proxyJump = "";
+      #   # proxyCommand = "";
+      #
+      #   #  match = ''
+      #   #  host  canonical
+      #   #  host  exec "ping -c1 -q 192.168.17.1"
+      #   # '';
+      #
+      #   # dynamicForwards  = [
+      #   #   {
+      #   #     "name" = {
+      #   #       address = "localhost";
+      #   #       port = 8080;
+      #   #     };
+      #   #   }
+      #   # ];
+      #
+      #   # remoteForwards = [
+      #   #   {
+      #   #     bind = {
+      #   #       address = "10.0.0.13";
+      #   #       port = 8080;
+      #   #     };
+      #   #     host = {
+      #   #       address = "10.0.0.13";
+      #   #       port = 80;
+      #   #     };
+      #   #   }
+      #   # ];
+      #
+      #   # localForwards = [
+      #   #   {
+      #   #     bind = {
+      #   #       address = "10.0.0.13";
+      #   #       port = "8080";
+      #   #     };
+      #   #     host = {
+      #   #       address = "10.0.0.13";
+      #   #       port = "80";
+      #   #     };
+      #   #   }
+      #   # ];
+      # };
     };
   };
 }
