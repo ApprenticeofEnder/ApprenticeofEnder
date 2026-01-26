8d85a31b110d7c82f131e0484a6d63775dde97d8
Robert Babaev <github@robertbabaev.tech>
2026-01-26 10:22:49 -0500

    build: add nur
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/nix.nix
+++ modules/home/nix.nix
@@ -12,9 +12,13 @@
       allowBroken = true;
     };
   };
+  nur = import flake.inputs.nur {
+    system = pkgs.stdenv.hostPlatform.system;
+  };
 in {
   # Make pkgs-unstable available to all modules
   _module.args.pkgs-unstable = pkgs-unstable;
+  _module.args.nur = nur;
 
   # `nix.package` is already set if on `NixOS` or `nix-darwin`.
   # TODO: Avoid setting `nix.package` in two places. Does https://github.com/juspay/nixos-unified-template/issues/93 help here?
