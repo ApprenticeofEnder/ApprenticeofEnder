f671ce9e6d16038da8ef45f26c250de6519c7b18
Robert Babaev <github@robertbabaev.tech>
2026-02-05 16:33:13 -0500

    feat: add python toolkit
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

---
+++ modules/home/toolkits/python.nix
@@ -0,0 +1,56 @@
+{pkgs, ...}: {
+  programs = {
+    ruff = {
+      enable = true;
+      settings = {
+        line-length = 80;
+        indent-width = 4;
+
+        lint = {
+          select = [
+            # pycodestyle
+            "E"
+            # Pyflakes
+            "F"
+            # pyupgrade
+            "UP"
+            # flake8-bugbear
+            "B"
+            # flake8-simplify
+            "SIM"
+            # isort
+            "I"
+            # fastapi
+            "FAST"
+          ];
+
+          extend-select = ["E501"];
+
+          isort.known-first-party = ["api" "tests"];
+
+          dummy-variable-rgx = "^(_+|(_+[a-zA-Z0-9_]*[a-zA-Z0-9]+?))$";
+        };
+
+        format = {
+          docstring-code-format = true;
+          docstring-code-line-length = "dynamic";
+        };
+      };
+    };
+    uv = {
+      enable = true;
+      settings = {};
+    };
+    vscode = {
+      profiles = {
+        default = {
+          extensions = with pkgs.vscode-extensions; [
+            ms-python.python
+            charliermarsh.ruff
+            detachhead.basedpyright
+          ];
+        };
+      };
+    };
+  };
+}
