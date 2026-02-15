1025ead6ab35aa54e35590762774dd786a0a5df5
Robert Babaev <github@robertbabaev.tech>
2026-02-15 10:02:01 -0500

    fix: disable broken spotify player
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/programs/spotify-player.nix
+++ modules/home/programs/spotify-player.nix
@@ -6,11 +6,13 @@
 lib.mkMerge [
   {
     programs.spotify-player = {
-      enable = true;
+      enable = lib.mkDefault true;
     };
   }
   (lib.mkIf pkgs.stdenv.isLinux {
     programs.spotify-player = {
+      # TODO: Re-enable this once the compiler error is fixed
+      enable = false;
       package = pkgs.spotify-player.override {
         withAudioBackend = "pulseaudio";
       };
