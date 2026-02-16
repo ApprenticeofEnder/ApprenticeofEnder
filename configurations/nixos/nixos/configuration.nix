0dc05e4240c85b49d61dc1fedd238a33205cc6a9
Robert Babaev <github@robertbabaev.tech>
2026-02-16 14:55:58 -0500

    feat: enable bluetooth
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- configurations/nixos/nixos/configuration.nix
+++ configurations/nixos/nixos/configuration.nix
@@ -40,6 +40,8 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
+  hardware.bluetooth.enable = true;
+
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
