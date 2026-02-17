565bb30a1fbb5183f1b9969cc7a25ef6e96d2952
Robert Babaev <github@robertbabaev.tech>
2026-02-17 11:04:04 -0500

    fix: update hostname
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

---
+++ configurations/nixos/ender-raptor/default.nix
@@ -0,0 +1,17 @@
+# See /modules/nixos/* for actual settings
+# This file is just *top-level* configuration.
+{flake, ...}: let
+  inherit (flake) inputs;
+  inherit (inputs) self;
+  homeMod = self + /modules/home;
+in {
+  imports = [
+    self.nixosModules.default
+    self.nixosModules._1password
+    ./configuration.nix
+  ];
+
+  home-manager.sharedModules = [
+    "${homeMod}/programs/linux-only"
+  ];
+}
