5089a3878e09eca49e4fdd0aab0fdea20ab38c39
Robert Babaev <github@robertbabaev.tech>
2026-02-19 12:39:29 -0500

    feat: new opencode permissions
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/programs/opencode/default.nix
+++ modules/home/programs/opencode/default.nix
@@ -1,27 +1,47 @@
 {...}: let
-  # lak = field: list:
-  #   builtins.listToAttrs (map (v: {
-  #       name = v.${field};
-  #       value = v;
-  #     })
-  #     list);
-  permissions = {
-    read = {
-      "*" = "allow";
-    };
-    edit = "ask";
-    bash = {
-      "*" = "ask";
-    };
-    webfetch = "ask";
-    "serena_*" = {
-      "*" = "ask";
-      "check_onboarding_performed" = "allow";
-      "find_*" = "allow";
-      "get_*" = "allow";
-      "initial_instructions" = "allow";
-    };
-  };
+  buildAccessList = accessValue: (
+    list:
+      builtins.listToAttrs (
+        map (
+          permission: {
+            name = permission;
+            value = accessValue;
+          }
+        )
+        list
+      )
+  );
+
+  toolList = prefix: (tools: (map (tool: "${prefix}_${tool}") tools));
+
+  serenaAllow = buildAccessList "allow" (
+    toolList "serena" [
+      "check_onboarding_performed"
+      "find_*"
+      "get_*"
+      "initial_instructions"
+      "list_*"
+      "read_*"
+      "search_*"
+    ]
+  );
+
+  permissions =
+    {
+      read = {
+        "*" = "allow";
+        "*.env" = "deny";
+      };
+      edit = "ask";
+      bash = {
+        "*" = "ask";
+      };
+
+      webfetch = "ask";
+
+      "serena_*" = "ask";
+    }
+    // serenaAllow;
 in {
   programs.opencode = {
     enable = true;
