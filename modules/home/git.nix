8545def656409a357add54c066e74693664d87c4
Robert Babaev <github@robertbabaev.tech>
2026-03-16 13:57:13 -0400

    fix: don't use difft on git by default
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -8,10 +8,10 @@
   programs = {
     difftastic = {
       enable = true;
-      git = {
-        enable = true;
-        diffToolMode = true;
-      };
+      # git = {
+      #   enable = true;
+      #   diffToolMode = true;
+      # };
     };
     git = {
       enable = true;
@@ -22,8 +22,13 @@
           name = config.me.fullname;
           email = config.me.email;
         };
-        alias = {
+        alias = let
+          prefix = "-c diff.external=difft";
+        in {
           ga = "git add .";
+          dl = "${prefix} log -p --ext-diff";
+          ds = "${prefix} show --ext-diff";
+          dft = "${prefix} diff";
         };
         # init.defaultBranch = "master";
         # pull.rebase = "false";
