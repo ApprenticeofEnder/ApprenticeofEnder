b09a563df5df3eaf563cd4efa0a674e22467d0ac
Robert Babaev <github@robertbabaev.tech>
2026-04-12 02:57:19 -0400

    fix: okay well the telescope settings didn't break things before...
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/programs/neovim/nixvim/plugins/telescope.nix
+++ modules/home/programs/neovim/nixvim/plugins/telescope.nix
@@ -7,29 +7,29 @@
       #   "themes"
       #   "terms"
       # ];
-      # settings = {
-      #   defaults = {
-      #     prompt_prefix = "   ";
-      #     selection_caret = " ";
-      #     entry_prefix = " ";
-      #     sorting_strategy = "ascending";
-      #     layout_config = {
-      #       horizontal = {
-      #         prompt_position = "top";
-      #         preview_width = 0.55;
-      #       };
-      #       width = 0.87;
-      #       height = 0.80;
-      #     };
-      #     mappings = {
-      #       n = {
-      #         "q" = {
-      #           __raw = ''require("telescope.actions").close'';
-      #         };
-      #       };
-      #     };
-      #   };
-      # };
+      settings = {
+        defaults = {
+          prompt_prefix = "   ";
+          selection_caret = " ";
+          entry_prefix = " ";
+          sorting_strategy = "ascending";
+          layout_config = {
+            horizontal = {
+              prompt_position = "top";
+              preview_width = 0.55;
+            };
+            width = 0.87;
+            height = 0.80;
+          };
+          mappings = {
+            n = {
+              "q" = {
+                __raw = ''require("telescope.actions").close'';
+              };
+            };
+          };
+        };
+      };
       # extensions_list = { "themes", "terms" },
       # extensions = {},
     };
