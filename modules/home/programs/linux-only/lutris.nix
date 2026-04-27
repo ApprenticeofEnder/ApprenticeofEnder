27664367afcf08e45d0fadb705083cd09dddd1e7
Robert Babaev <github@robertbabaev.tech>
2026-04-27 09:26:28 -0400

    fix: lutris openldap had a failed test on unstable
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/programs/linux-only/lutris.nix
+++ modules/home/programs/linux-only/lutris.nix
@@ -1,6 +1,11 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  pkgs-stable,
+  ...
+}: {
   programs.lutris = {
     enable = true;
+    package = pkgs-stable.lutris;
     winePackages = with pkgs; [wineWow64Packages.full];
   };
 }
