4312e7ed66b4abb62f9efe657934a515b54cb022
Robert Babaev <github@robertbabaev.tech>
2026-04-27 09:53:10 -0400

    feat: activate terramaid on nixos and nix-darwin
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -1,6 +1,15 @@
-{
+{flake, ...}: let
+  terramaidOverlay = _: prev: let
+    system = prev.stdenv.hostPlatform.system;
+  in {
+    terramaid = flake.inputs.Terramaid.packages.${system}.default;
+  };
+in {
   imports = [
     ./myusers.nix
     ./packages.nix
+  ];
+  nixpkgs.overlays = [
+    terramaidOverlay
   ];
 }
