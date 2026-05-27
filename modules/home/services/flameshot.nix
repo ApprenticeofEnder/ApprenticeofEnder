07c3304fb73a5d2baabfe424cb67e9035efa5f6f
Robert Babaev <github@robertbabaev.tech>
2026-05-26 21:30:25 -0400

    fix(services): reverted flameshot to 12.1
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/services/flameshot.nix
+++ modules/home/services/flameshot.nix
@@ -1,5 +1,15 @@
-{
+{pkgs, ...}: let
+  flameshot-pkgs =
+    import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/05bbf675397d5366259409139039af8077d695ce.tar.gz";
+      sha256 = "sha256-IE7PZn9bSjxI4/MugjAEx49oPoxu0uKXdfC+X7HcRuQ=";
+    }) {
+      system = pkgs.stdenv.hostPlatform.system;
+    };
+  flameshot = flameshot-pkgs.flameshot;
+in {
   services.flameshot = {
     enable = true;
+    package = flameshot;
   };
 }
