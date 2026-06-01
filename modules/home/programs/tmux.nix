ec36f5fb0395268eb63a7ed59b37666f98095078
Robert Babaev <github@robertbabaev.tech>
2026-06-01 13:32:43 -0400

    fix(tmux): add starter claude code config
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/programs/tmux.nix
+++ modules/home/programs/tmux.nix
@@ -7,5 +7,10 @@
     enable = true;
     keyMode = "vi";
     shell = "${lib.getExe pkgs.fish}";
+    extraConfig = ''
+      set -g allow-passthrough on
+      set -s extended-keys on
+      set -as terminal-features 'xterm*:extkeys'
+    '';
   };
 }
