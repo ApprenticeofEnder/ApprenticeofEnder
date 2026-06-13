ad84b34d1247d082816775d5f3b8710a58ed6efe
Robert Babaev <github@robertbabaev.tech>
2026-06-13 10:33:57 -0400

    feat: add cybersci vpns
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- configurations/nixos/ender-raptor/default.nix
+++ configurations/nixos/ender-raptor/default.nix
@@ -1,11 +1,58 @@
 # See /modules/nixos/* for actual settings
 # This file is just *top-level* configuration.
-{flake, ...}: let
+{
+  flake,
+  lib,
+  ...
+}: let
   inherit (flake) inputs;
   inherit (inputs) self;
   inherit (inputs) nixvim;
   inherit (inputs) stylix;
   homeMod = self + /modules/home;
+
+  vpn_root = "/home/ender/Documents/VPNs";
+
+  cybersci = {
+    events = {
+      ntl = "n";
+      rgnl = "r";
+    };
+  };
+
+  trimYear = year: builtins.substring 2 4 year;
+
+  mkVpn = name: {
+    config_path,
+    auto_start ? false,
+  }:
+    lib.nameValuePair name {
+      config = ''config ${config_path}'';
+      autoStart = auto_start;
+    };
+
+  mkCyberSciVpn = {
+    event,
+    year,
+    teams ? [],
+    dev_config,
+  }: let
+    event_prefix = builtins.concatStringsSep "" [
+      cybersci.events."${event}"
+      (trimYear year)
+    ];
+    prefix = "cybersci-${event_prefix}";
+    dev_vpn = mkVpn "${prefix}-dev" {
+      config_path = dev_config;
+    };
+
+    defence = map (team:
+      mkVpn "${prefix}-${team}-defence" {
+        config_path = "${vpn_root}/${event_prefix}/cybersci-${event}-${team}.ovpn";
+      })
+    teams;
+  in
+    builtins.listToAttrs ([dev_vpn] ++ defence);
 in {
   imports = [
     self.nixosModules.default
@@ -26,9 +73,24 @@ in {
 
   programs.nix-ld.enable = true;
 
-  services.openvpn.servers = {
-    cybersci = {config = ''config /home/ender/Projects/CyberSci/CS2026-NTL/infrastructure/cybersci-ntl-dev.ovpn '';};
-  };
+  services.openvpn.servers = lib.mergeAttrsList [
+    (mkCyberSciVpn {
+      event = "ntl";
+      year = "2026";
+      dev_config = "/home/ender/Projects/CyberSci/CS2026-NTL/infrastructure/cybersci-ntl-dev.ovpn";
+      teams = [
+        "club"
+        "gogo"
+        "malware"
+        "maple"
+        "offby"
+        "pada"
+        "sherbrooke"
+        "status418"
+        "t0ken"
+      ];
+    })
+  ];
 
   # Automatically move old dotfiles out of the way
   #
