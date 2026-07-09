237a3c89ced3131ebf5e19bdb669af137d0ab0e9
Robert Babaev <github@robertbabaev.tech>
2026-07-09 10:43:23 -0400

    feat(neovim): add modal for opening editor logs
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- dotfiles/nvim/.config/nvim/lua/experimental.lua
+++ dotfiles/nvim/.config/nvim/lua/experimental.lua
@@ -7,3 +7,32 @@
 -- 		vim.fn.setreg("s", ":!keep-sorted " .. vim.api.nvim_buf_get_name(0) .. enter)
 -- 	end,
 -- })
+
+function Open_file_in_modal(file)
+	Snacks.win({
+		file = file,
+		width = 0.6,
+		height = 0.6,
+		wo = {
+			spell = false,
+			wrap = true,
+			signcolumn = "yes",
+			statuscolumn = " ",
+			conceallevel = 3,
+		},
+	})
+end
+
+local nvim_data = vim.fn.expand("$HOME/.local/state/nvim")
+
+vim.keymap.set({ "n" }, "<leader>ln", function()
+	Open_file_in_modal(require("vim.lsp.log").get_filename())
+end, { desc = "Open Neovim logs" })
+
+vim.keymap.set({ "n" }, "<leader>lf", function()
+	Open_file_in_modal(string.format("%s/conform.log", nvim_data))
+end, { desc = "Open Conform logs" })
+
+vim.keymap.set({ "n" }, "<leader>ll", function()
+	Open_file_in_modal(string.format("%s/lsp.log", nvim_data))
+end, { desc = "Open LSP logs" })
