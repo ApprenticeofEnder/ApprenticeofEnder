343f4da23970871b2e2fa6ccc70799029442b490
Robert Babaev <github@robertbabaev.tech>
2026-07-16 09:48:44 -0400

    chore: pulumi esc retired
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -41,7 +41,6 @@
     # keep-sorted start
     gnutls
     nmap # recon
-    pulumi-esc # secrets management
     ripsecrets
     # _1password-gui # TODO: Work out the whole user/group requirement thing
     # _1password-cli
