9cefc033975af9c9483e1919a58d3819de7f6494
Robert Babaev <github@robertbabaev.tech>
2026-08-14 12:25:36 -0400

    feat(services/espanso): add email alias + additional date formats
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/services/espanso.nix
+++ modules/home/services/espanso.nix
@@ -4,12 +4,41 @@
       trigger = ":date";
       replace = "{{currentdate}}";
     }
+    {
+      trigger = ":datemdy";
+      replace = "{{currentdate_mdy}}";
+    }
+    {
+      trigger = ":datedmy";
+      replace = "{{currentdate_dmy}}";
+    }
+    {
+      trigger = ":ealias";
+      form = ''
+        [[username]]@[[domain]]
+      '';
+      form_fields = {
+        domain = {
+          default = "public.robertbabaev.tech";
+        };
+      };
+    }
   ];
   global_vars = [
     {
       name = "currentdate";
       type = "date";
       params = {format = "%Y-%m-%d";};
+    }
+    {
+      name = "currentdate_mdy";
+      type = "date";
+      params = {format = "%m/%d/%Y";};
+    }
+    {
+      name = "currentdate_dmy";
+      type = "date";
+      params = {format = "%d/%m/%Y";};
     }
   ];
 in {
