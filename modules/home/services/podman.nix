96b1c84a92df693e43f7eb45466a2e76988e1666
Robert Babaev <github@robertbabaev.tech>
2026-09-02 11:35:47 -0400

    fix(podman): add DOCKER_HOST session variable
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- modules/home/services/podman.nix
+++ modules/home/services/podman.nix
@@ -1,4 +1,7 @@
 {pkgs, ...}: {
+  home.sessionVariables = {
+    DOCKER_HOST = "unix://$XDG_RUNTIME_DIR/podman/podman.sock";
+  };
   services.podman = {
     enable = pkgs.stdenv.hostPlatform.isLinux;
     enableTypeChecks = true;
