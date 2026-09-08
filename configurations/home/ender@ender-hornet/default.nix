b87f8af902f55358c348513cd7645e7b3d3600cf
Robert Babaev <github@robertbabaev.tech>
2026-09-08 09:23:24 -0400

    Revert "feat: add kind quadlet"
    This reverts commit 51e41aecdac5406379a976722c8b87f76456e2e4.

--- configurations/home/ender@ender-hornet/default.nix
+++ configurations/home/ender@ender-hornet/default.nix
@@ -52,39 +52,6 @@ in {
 
   podman.enable = true;
 
-  services.podman.containers = {
-    kind-hornet-control-plane = {
-      autoStart = true;
-      image = "docker.io/kindest/node@sha256:3489c7674813ba5d8b1a9977baea8a6e553784dab7b84759d1014dbd78f7ebd5";
-      ports = [
-        "40427:6443"
-      ];
-      volumes = [
-        "a8214781454be5e6d80a4f135d4cfb0aade9689c57976db724c4a9ef99e0c018:/var:suid,exec,dev"
-        "/lib/modules:/lib/modules:ro"
-        "/dev/mapper:/dev/mapper"
-      ];
-      devices = [
-        "/dev/fuse"
-      ];
-      labels = {
-        "io.x-k8s.kind.role" = "control-plane";
-        "io.x-k8s.kind.cluster" = "kind-hornet";
-      };
-      environment = {
-        container = "podman";
-        KUBECONFIG = "/etc/kubernetes/admin.conf";
-      };
-      network = "kind";
-      extraPodmanArgs = [
-        "--tmpfs"
-        "/tmp"
-        "--tmpfs"
-        "/run"
-      ];
-    };
-  };
-
   toolkits.ai-server = {
     enable = true;
 
